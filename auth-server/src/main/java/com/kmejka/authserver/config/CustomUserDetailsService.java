3e918b7eecbb1c5da51bb307433d4c7a07c0241a
rtatol <rtatol@gmail.com>
2018-06-15 19:53:48 +0200

    replace com.sun.tools.javac.util.List with java.util.Arrays

--- auth-server/src/main/java/com/kmejka/authserver/config/CustomUserDetailsService.java
+++ auth-server/src/main/java/com/kmejka/authserver/config/CustomUserDetailsService.java
@@ -1,8 +1,8 @@
 package com.kmejka.authserver.config;
 
+import java.util.Arrays;
 import java.util.Collection;
 
-import com.sun.tools.javac.util.List;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -16,7 +16,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
 
-        final Collection<SimpleGrantedAuthority> authorities = List.of(
+        final Collection<SimpleGrantedAuthority> authorities = Arrays.asList(
                 new SimpleGrantedAuthority("ROLE_USER"),
                 new SimpleGrantedAuthority("ROLE_INTERNAL_USER"));
         return new User(username, "admin",true, true, true, true, authorities);
